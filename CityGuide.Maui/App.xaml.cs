2ca60fca8061dd2493dbe9ae1d3c355ad0763717
NurayMercan26 <nuraymercan26@gmail.com>
2026-07-07 11:28:22 +0300

    committransport

--- CityGuide.Maui/App.xaml.cs
+++ CityGuide.Maui/App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new Views.WelcomePage());
+            return new Window(new Views.TransportationPage());
         }
     }
 }
